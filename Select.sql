a9c8104264f169748910eb835129bffa89365fc2
Raghuveer Venkatesh <raghuveervenkatesh7@gmail.com>
2024-08-28 19:53:27 +0530

    Push initial

--- Select.sql
+++ Select.sql
@@ -2,7 +2,7 @@
 # 1757. Recyclable and Low Fat Products 
 select product_id from Products where low_fats = 'Y' and recyclable = 'Y'
 
-# 584. Find Customer Referee
+# 584. Find Customer Referee 
 select name from Customer where referee_id <> '2' or referee_id is null;
 # Learnt about null statements here
 
