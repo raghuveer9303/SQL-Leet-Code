4f0c5060d8f0eacc29ba82e2537894a1c96f1631
Raghuveer Venkatesh <raghuveervenkatesh7@gmail.com>
2024-08-28 19:52:39 +0530

    First Push

--- Select.sql
+++ Select.sql
@@ -1,5 +1,5 @@
 
-# 1757. Recyclable and Low Fat Products   
+# 1757. Recyclable and Low Fat Products 
 select product_id from Products where low_fats = 'Y' and recyclable = 'Y'
 
 # 584. Find Customer Referee
