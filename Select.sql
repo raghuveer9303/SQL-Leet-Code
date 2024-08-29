eaf8e8bc1d4e209f025b8f7c6a8e8a0b750a0a02
Raghuveer Venkatesh <raghuveervenkatesh7@gmail.com>
2024-08-29 20:29:50 +0530

    Completed 2 more problems on 29-08

--- Select.sql
+++ Select.sql
@@ -6,5 +6,9 @@ select product_id from Products where low_fats = 'Y' and recyclable = 'Y'
 select name from Customer where referee_id <> '2' or referee_id is null;
 # Learnt about null statements here 
 
+# 595. Big Countries
+select name, population, area from World where area >= 3000000 or population >= 25000000
 
+# 1148. Article Views is
+select distinct author_id as id from Views where author_id = viewer_id order by id
 
