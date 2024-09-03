66006b607c09a33f73048516cc318e3a0d8293ce
raghuveer9303 <34271141+raghuveer9303@users.noreply.github.com>
2024-09-03 09:02:53 +0530

    Added another question on joins

--- Basic Joins.sql
+++ Basic Joins.sql
@@ -3,3 +3,7 @@ select EmployeeUNI.unique_id, Employees.name
 from Employees
 left join EmployeeUNI on Employees.id = EmployeeUNI.id
 
+# 1068. Product Sales Analysis I
+select Product.product_name, Sales.year, Sales.price 
+from Sales 
+left join Product on Sales.product_id = Product.product_id
