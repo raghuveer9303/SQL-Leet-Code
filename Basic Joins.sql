bb0184eae6b35c0d5110a71a0e00cb662df531d7
raghuveer9303 <34271141+raghuveer9303@users.noreply.github.com>
2024-09-05 22:07:30 +0530

    Added another basic join file

--- Basic Joins.sql
+++ Basic Joins.sql
@@ -7,3 +7,17 @@ left join EmployeeUNI on Employees.id = EmployeeUNI.id
 select Product.product_name, Sales.year, Sales.price 
 from Sales 
 left join Product on Sales.product_id = Product.product_id
+
+# 1581. Customer Who Visited but Did Not Make Any Transactions
+SELECT 
+  v.customer_id, 
+  COUNT(t.visit_id) AS count_no_trans 
+FROM 
+  Visits AS v 
+  LEFT JOIN Transactions AS t ON v.visit_id = t.visit_id 
+GROUP BY 
+  v.customer_id
+HAVING 
+  COUNT(t.visit_id) > 0;  -- Exclude customers with 0 transactions
+
+  
