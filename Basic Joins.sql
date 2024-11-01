2e77b04c0834949bba70c0a9701f0620863d1fbb
raghuveer9303 <34271141+raghuveer9303@users.noreply.github.com>
2024-11-01 13:08:08 -0400

    Studeents and Examinations

--- Basic Joins.sql
+++ Basic Joins.sql
@@ -55,3 +55,13 @@ Employee AS E
 LEFT JOIN Bonus AS B
 ON E.empId = B.empId
 WHERE B.bonus < 1000 OR B.bonus IS NULL;
+
+# 1280. Students and Examinations
+
+SELECT S.student_id, S.student_name, Sub.subject_name, COALESCE(COUNT(E.student_id),0) AS attended_exams
+FROM Students AS S
+CROSS JOIN Subjects AS Sub
+LEFT JOIN Examinations AS E
+ON S.student_id = E.student_id AND Sub.subject_name = E.subject_name
+GROUP BY S.student_id, S.student_name, Sub.subject_name
+ORDER BY S.student_id, Sub.subject_name
