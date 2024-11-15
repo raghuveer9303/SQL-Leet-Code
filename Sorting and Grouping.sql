d770ac9073d82e876ececa7baf10ef6485453e57
raghuveer9303 <34271141+raghuveer9303@users.noreply.github.com>
2024-11-15 10:45:08 -0500

    User Activity for the past 30 days 2

--- Sorting and Grouping.sql
+++ Sorting and Grouping.sql
@@ -3,3 +3,11 @@
 SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt 
 FROM Teacher
 GROUP BY teacher_id
+
+# 1141. User Activity for the Past 30 Days I
+
+SELECT activity_date as day, COUNT(DISTINCT user_id) as active_users
+FROM Activity
+WHERE datediff('2019-07-27',activity_date)<30 
+AND activity_date <= '2019-07-27' 
+GROUP BY activity_date
