9b5e61328313863432a4173f582a66899f954f06
raghuveer9303 <34271141+raghuveer9303@users.noreply.github.com>
2025-02-10 18:21:47 -0500

    1491

---
+++ Python_Leet_Code/1491.py
@@ -0,0 +1,6 @@
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        salary.sort()
+        total = len(salary) - 2 
+        avg = sum(salary[1:-1])/total
+        return avg
